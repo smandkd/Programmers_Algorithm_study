ecf542f7d6802a6a36f9cfd5a893ea2d07ace77e
Sang Min Oh <50042497+smandkd@users.noreply.github.com>
2023-07-05 08:36:42 +0900

    Create flag에따라다른값반환하기.java

---
+++ flag에따라다른값반환하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(a: Int, b: Int, flag: Boolean): Int {
+        var answer: Int = 0
+        
+        if(flag == true) {
+            return a + b
+        }
+        else {
+            return a - b
+        }
+        
+    }
+}
