0e340b4f9895ca64b949bb1f16373cd120d6b33a
Sang Min Oh <50042497+smandkd@users.noreply.github.com>
2023-07-10 16:06:25 +0900

    Update 문장속단어.kt

--- 문장속단어.kt
+++ 문장속단어.kt
@@ -24,3 +24,33 @@ class Main{
         println(T.solution(str));
     }
 }
+
+class Main{
+    fun solution(str: String) : String {
+        var answer = ""
+        var m = Int.MIN_VALUE
+        var pos = 0
+
+        while((pos=str.split(" ").indexOf()) != -1) {
+            var t = str.substring(0, pos)
+            var len = t.length()
+            if(len > m) {
+                m = len
+                answer = t
+            }
+
+            str = str.substring(pos+1)
+        }
+
+        if(str.length > m) answer = str
+
+        return t
+    }
+
+    fun main(args: Array<String>) {
+        val T = Main();
+        val scanner = Scanner(System.in)
+        val str = scanner.nextLine()
+        println(T.solution(str))
+    }
+}
