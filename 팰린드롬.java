428a6164c54f1e12d1dd6bbb528b7f47e1d57d59
Sang Min Oh <50042497+smandkd@users.noreply.github.com>
2023-07-12 09:28:28 +0900

    Create 팰린드롬.java

---
+++ 팰린드롬.java
@@ -0,0 +1,26 @@
+// tip: each public class is put in its own file
+import java.util.Scanner;
+import java.util.*;
+
+class Main
+{
+    public String solution(String str) {
+        String answer = "NO";
+
+        String s = str.toUpperCase().replaceAll("[^A-Z]", "");
+        String tmp = new StringBuilder(s).reverse().toString();
+
+        if(s.equals(tmp)) answer = "YES";
+
+        return answer;
+    }
+
+    // tip: arguments are passed via the field below this editor
+    public static void main(String[] args)
+    {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        System.out.println(T.solution(str));
+    }
+}
